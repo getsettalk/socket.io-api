58397e2322fd85aba873ddac2c2a5ea733e4a723
sujeet kumar <rajrock7254@gnail.com>
2022-11-04 13:42:29 -0400

    added realtime listener for send and receive message

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const http = require('http');
 const server = http.createServer(app);
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
-const io = require('socket.io')(server,{
-  cors:{
-      origin:'*',
-      methods:['GET','POST']
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
   }
 });
 app.use(cors());
@@ -15,33 +15,44 @@ app.use(cors());
 
 
 app.use(express.static('public'))
-app.get('/',(req,res)=>{
-    // res.sendFile(__dirname+'/index.html');
-    res.send("Server is running")
+app.get('/', (req, res) => {
+  // res.sendFile(__dirname+'/index.html');
+  res.send("Server is running")
 
 })
 var totalUser = 0;
 var userLIST = {};
 io.on('connection', (socket) => {
-    console.log('Coming room id ',socket.id);
-    var roomid = socket.id;
-    totalUser = totalUser+1;
-    io.emit('status', totalUser);
-    console.log('a user connected', totalUser);
-    
-    // join particular roojm 
-    socket.join(roomid)
-    userLIST = {...userLIST,roomid}
-    io.to(roomid).emit('joinRoom',({YoUroomid:roomid,uList:userLIST})) 
-     console.log(userLIST)
-    socket.on('disconnect', (sid) => {
-      console.log(sid)
-      console.log('user disconnected', totalUser);
-      totalUser = totalUser-1;
-      io.emit('status', totalUser);
-      socket.broadcast.emit('Discuser', 'A new user has been disconnected ?');
-    });
-  });
- 
+  console.log('Coming room id ', socket.id);
+  var roomid = socket.id;
+  totalUser = totalUser + 1;
+  io.emit('status', totalUser);
+  console.log('a user connected', totalUser);
 
-server.listen(PORT,()=> console.log(` Listining at Port ${PORT}`))
+
+  userLIST = { ...userLIST, roomid }
+  console.log(userLIST)
+
+  // listen and send
+  socket.on('chatMessage1', (msg) => {
+    // console.log('message: ' + msg);
+    socket.broadcast.emit('chatMessage2', msg);
+    // io.emit('chat message2', msg);
+  });
+
+  socket.on("TypingStatus", (data) => { // typing status
+    socket.broadcast.emit("getTypingStatus", data)
+    // console.log("Typing:"+data.TypingUsername)
+  })
+
+  socket.on('disconnect', (sid) => {
+    console.log(sid)
+    console.log('user disconnected', totalUser);
+    totalUser = totalUser - 1;
+    io.emit('status', totalUser);
+    socket.broadcast.emit('Discuser', 'A new user has been disconnected ?');
+  });
+});
+
+
+server.listen(PORT, () => console.log(` Listining at Port ${PORT}`))
